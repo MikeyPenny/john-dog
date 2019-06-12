0eeafa700b620e22615db6049cad913ede1bf67a
Miguel Sandoval <miguelship3@gmail.com>
2019-06-12 17:14:52 +0200

    double declaration

--- api/app.js
+++ api/app.js
@@ -12,7 +12,6 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT;
 
 var favicon = require('serve-favicon')
-var path = require('path')
  
 var app = express()
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
