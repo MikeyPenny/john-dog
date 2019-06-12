b595c3a42058dc45d1d522952908cbdf369001c9
Miguel Sandoval <miguelship3@gmail.com>
2019-06-12 17:39:23 +0200

    please?

--- api/public/build/precache-manifest.5be16f8e6a0d2fbfc577c4cd93bf91cf.js
+++ api/public/build/precache-manifest.5be16f8e6a0d2fbfc577c4cd93bf91cf.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "22f68db5857cfd3dab39fca8992954c7",
+    "revision": "9ec982dd756dd3ba36f997b37f2440ea",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.62908225.chunk.css"
   },
   {
-    "revision": "30bdded11f86ee10d870",
+    "revision": "ed12e2bc5b24fd95fc57",
     "url": "/static/css/main.cba710d6.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.45bb56ab.chunk.js"
   },
   {
-    "revision": "30bdded11f86ee10d870",
-    "url": "/static/js/main.6bb8476a.chunk.js"
+    "revision": "ed12e2bc5b24fd95fc57",
+    "url": "/static/js/main.8ac097a8.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
